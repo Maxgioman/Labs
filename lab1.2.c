4af628367b6e4b209aad0b6a82eb8ac2e4254b0e
Max Pulyk <43215127+Maxgioman@users.noreply.github.com>
2018-09-26 17:07:28 +0300

    Add files via upload

---
+++ lab1.2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+    
+int n, m;
+    
+/* Inputing*/
+    
+printf("Give me two numbers\n");
+    
+scanf("%i %i",&n, &m);
+    
+/* First task*/
+    
+printf("Answers:\n");
+    
+printf("%i\n", n - --m );
+    
+/* Second task*/
+    
+printf("%s\n", m--<n?"True":"False");
+    
+/* Third task*/
+    
+printf("%s\n", n++>m?"True":"False");
+
+    
+return 0;
+
+}
