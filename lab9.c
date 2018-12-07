b54eb58a20015dc0486255c97fe7948d6c240baa
Max Pulyk <43215127+Maxgioman@users.noreply.github.com>
2018-12-07 11:41:12 +0200

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,107 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
 
-void copyFile(FILE* fp,FILE* fpout);
-void Print(FILE* fp);
-void maxcon(FILE* fp);
-void putTextInFile(FILE* fp);
-
-int main(int argc, char* argv[])
+int main()
 {
-	FILE* fp = fopen("lab9.txt","w");
-	FILE* fpout = fopen("lab9out.txt","w");
-	if(fp == NULL || fpout == NULL)
-		return -1;
+    int length,i,j;
 
-    putTextInFile(fp);
-    freopen("lab9.txt","r",fp);
-	copyFile(fp,fpout);
-    freopen("lab9out.txt","r",fpout);
-	printf("\nStrings of start file:\n");
-	Print(fp);
-	printf("\nStrings after copying in second file:\n");
-	Print(fpout);
-	freopen("lab9out.txt","r",fpout);
-	maxcon(fpout);
-	fclose(fp);
-	fclose(fpout);
-	return 0;
-}
+    printf("Enter the size of your array: ");
 
-void copyFile(FILE* fp,FILE* fpout)
-{
-	char buffer[255];
-	int N1,N2;
-	printf("\nLines with only one word\n");
-	int count = 1;
-	while(fgets(buffer,sizeof(buffer),fp))
-	{
-		int k = strlen(buffer);
-		if(buffer[k] == '\n')
-			k--;
-		if(buffer[k] != ' ')
-		{
-			fputs(buffer,fpout);
-		}
-		count++;
-	}
-}
+    scanf("%i",&length);
 
-void Print(FILE* fp)
-{
-	fseek (fp,0,SEEK_SET);
-	fgetpos(fp,0);
-	char* buffer = NULL;
-	while(fgets(buffer,sizeof(buffer),fp))
-	{
-		printf("%s",buffer);
-	}
-}
+    float* mas = (float*) malloc(sizeof(float)*length);
 
-void maxcon(FILE* fp)
-{
-	if(fp == NULL)
-		exit(-1);
-	int max;
-	int tempcount = 0;
-	int tempi = 1;;
-	char buffer1[255];
-	while(fgets(buffer1,sizeof(buffer1),fp))
-	{
-		int count = 0;
-		char vowels[12] = {'a','e','i','o','u','y'};
-		for(int i = 0; buffer1[i]!= '\0';i++)
-		{
-		if(buffer1[i] != 'a'&&buffer1[i] != 'e'&&buffer1[i] != 'i'&&buffer1[i] != 'o'&&buffer1[i] != 'u'&&buffer1[i] != 'y')
-			count++;
-		}
-		if(count > tempcount)
-		{
-			tempcount = count;
-			max = tempi;
-		}
-		tempi++;
-	}
-	if(max != 0)
-		printf("\n\nString with number %d has the most consonant characters",max);
-	else
-		printf("There is not string with consonants");
-	fclose(fp);
-}
-void putTextInFile(FILE* fp)
-{
-	int n;
-	printf("How many lines do u want to write?\n");
-	scanf("%d",&n);
-	for(int i = 0; i < n+1;i++)
-	{
-		char tempbuffer[255];
-		fgets(tempbuffer);
-		if(i != 0)
-		{
-			fputs(tempbuffer,fp);
-			fputc('\n',fp);
-		}
-	}
+    printf("Start filling your array: \n");
+    for (i=0;i<length;i++)
+    {
+        printf("Element[%i] = ",i+1);
+        scanf("%f",&mas[i]);
+    }
+
+    float** mas2;
+
+    mas2 = (float**)malloc(sizeof(float*)*length);
+    for(int k = 0; k<length;k++)
+    mas2[k] = (float*)malloc(sizeof(float)*length);
+
+    for (i=0;i<length;i++)
+        for (j=0;j<length;j++)
+            {
+                mas2[j][i] = mas[i] / length;
+            }
+
+    for (i=0;i<length;i++)
+        for (j=0;j<length;j++)
+        {
+            printf("Element[%i][%i] = %.2f\n",i+1,j+1,mas2[i][j]);
+        }
+
+        free(mas);
+        free(mas2);
+
+        return 0;
 }
